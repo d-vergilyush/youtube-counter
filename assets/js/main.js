2db29f1183703dd3373fb51e784e845f0a6ab87b
Dana <d-vergilyush@bk.ru>
2018-10-12 23:18:40 +0300

    async Ajax call to  the server is written

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
-"use strict";
+'use strict';
 
-var a = 222;
+var HttpClient = function HttpClient() {
+  this.get = function (aUrl, aCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) aCallback(xhr.responseText);
+    };
+
+    xhr.open("GET", aUrl, true);
+    xhr.send(null);
+  };
+};
+
+var client = new HttpClient();
+
+var showStat = function showStat() {
+  client.get('https://www.googleapis.com/youtube/v3/channels?part=brandingSettings,statistics&id=UC33CqCsFPEHuwhpaES7ORpQ&key=AIzaSyDHfcVmwozE0iZfO65NLegrqpXNis2I7aE', function (response) {
+    var subsCount = JSON.parse(response).items[0].statistics.subscriberCount;
+    var viewCount = JSON.parse(response).items[0].statistics.viewCount;
+    var channelDescr = JSON.parse(response).items[0].brandingSettings.channel.description;
+    document.querySelector('.subs-count').innerHTML = subsCount;
+    document.querySelector('.view-count').innerHTML = viewCount;
+    document.querySelector('.channel-description').innerHTML = channelDescr;
+    //
+
+
+    console.log(JSON.parse(response));
+  });
+};
+
+showStat();
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
-let a = 222;
+   let HttpClient = function () {
+     this.get = function (aUrl, aCallback) {
+       let xhr = new XMLHttpRequest();
+       xhr.onreadystatechange = function () {
+         if (xhr.readyState == 4 && xhr.status == 200)
+           aCallback(xhr.responseText);
+       }
+
+       xhr.open("GET", aUrl, true);
+       xhr.send(null);
+     }
+   }
+
+  let client = new HttpClient();
+
+
+   let showStat = () => {
+     client.get(`https://www.googleapis.com/youtube/v3/channels?part=brandingSettings,statistics&id=UC33CqCsFPEHuwhpaES7ORpQ&key=AIzaSyDHfcVmwozE0iZfO65NLegrqpXNis2I7aE`, (response) => {
+     let subsCount = JSON.parse(response).items[0].statistics.subscriberCount;
+     let viewCount = JSON.parse(response).items[0].statistics.viewCount;
+     let channelDescr = JSON.parse(response).items[0].brandingSettings.channel.description;
+     document.querySelector('.subs-count').innerHTML = subsCount;
+     document.querySelector('.view-count').innerHTML = viewCount;
+     document.querySelector('.channel-description').innerHTML = channelDescr;
+    //
+
+
+    console.log(JSON.parse(response));
+     });
+   };
+
+showStat();
